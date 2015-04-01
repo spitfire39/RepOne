297116d51effbe2d7099a38b6801d2ad1c137e23
spitfire39 <vladimir.korbach@gmail.com>
2015-04-01 21:05:42 +0300

    Push all changes from from fifth lesson

--- src/com/company/OpenChromeBrowser.java
+++ src/com/company/OpenChromeBrowser.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class OpenChromeBrowser
@@ -26,13 +27,34 @@ public class OpenChromeBrowser
         googleSearch.sendKeys("qa factory", Keys.ENTER);
         Thread.sleep(5000);
         //googleSearch.submit();
-        // --- find some ID in rearch results
+        // --- find some ID in search results
         WebElement searchResult = driver.findElement(By.id("rso")); //*[@id="rso"]
 
         WebElement linkText = searchResult.findElement(By.tagName("a"));
         System.out.println(linkText.getText());
 
+        // --- create new "list" which will return all data from search request
+        // --- using for
+        List<WebElement> listOfElements = searchResult.findElements(By.tagName("a"));
+        for (int i = 0; i < listOfElements.size(); i++)
+        {
+            System.out.println(listOfElements.get(i).getText());
+        }
 
+        // --- create new "list" which will return all data from search request
+        // --- using while
+        int j = 0;
+        int size = listOfElements.size();
+        while (j < size)
+        {
+            System.out.println(listOfElements.get(j).getText());
+            j = j+1;
+        }
+
+        googleSearch.clear();
+        googleSearch.sendKeys("enstein");
+        WebElement btn = driver.findElement(By.name("btnG"));
+        btn.click();
         // --- wait for xxxx mili seconds
         Thread.sleep(5000);
         // --- close Chrome
---
+++ src/com/company/OperatorLists.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperatorLists
+{
+    public static void main(String[] args)
+    {
+        // --- this is a massive rows
+        String [] mass1 = {"Hello ", "Class", "."};
+        System.out.println(mass1[0]);
+        System.out.println(mass1[1]);
+        System.out.println(mass1[2]);
+
+        // --- this is a list of rows
+        List<String> newList = new ArrayList<String>();
+        // --- add to list some values
+        newList.add("Hello ");
+        newList.add("Class");
+        newList.add(".");
+        // --- display values form the List
+        System.out.println(newList.get(0));
+        System.out.println(newList.get(1));
+        System.out.println(newList.get(2));
+    }
+}
