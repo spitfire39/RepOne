641af828c48617f2e2ffd07243fcd4275ee14838
spitfire39 <vladimir.korbach@gmail.com>
2015-07-14 14:16:31 +0300

    Update TestLoadTime.java

--- src/blueworkslive/TestLoadTime.java
+++ src/blueworkslive/TestLoadTime.java
@@ -7,12 +7,13 @@ import org.junit.Test;
 
 public class TestLoadTime {
     @Test
+    //--- update path to ChromeDriver before execute
     public void LoadTime() throws Exception {
         ObjectLogin o = new ObjectLogin("https://www.blueworkslive.com/"
                 , "c:/Work/workspace/WebDriverProject/chromedriver.exe"
                 , "automation.user.smtp@gmail.com"
                 , "qwerty1234");
-        long startTime = System.currentTimeMillis()/1000;
+        //--- get laoding time for each tab
         o.getWorkTab();
         o.getCommunityTab();
         o.getLibraryTab();
