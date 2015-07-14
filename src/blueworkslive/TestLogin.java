450b53d1a609ef1519055f046c9edc73ee2fd149
spitfire39 <vladimir.korbach@gmail.com>
2015-07-14 14:18:15 +0300

    Update TestLogin.java

--- src/blueworkslive/TestLogin.java
+++ src/blueworkslive/TestLogin.java
@@ -6,12 +6,16 @@ import org.junit.Test;
 
 public class TestLogin {
     @Test
+    //--- update path to ChromeDriver before execute
     public void login() throws Exception {
+        //--- execution of login actions
         ObjectLogin o = new ObjectLogin("https://www.blueworkslive.com/"
                 , "c:/Work/workspace/WebDriverProject/chromedriver.exe"
                 , "automation.user.smtp@gmail.com"
                 , "qwerty1234");
+        //--- verification that we've login by corresponding user
         Assert.assertEquals("automation.user.smtp@gmail.com", o.getAdminInfo());
+        //--- log out
         o.pressLogOut();
     }
     @After
